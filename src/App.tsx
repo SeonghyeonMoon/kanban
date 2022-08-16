b089820e3647b1dc0d776786713fe7d8a70b15cd
Seonghyeon Moon <shine7329@gmail.com>
2022-08-16 09:16:58 +0900

    [feat] bookmark initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Bookmark from './components/bookmark/Bookmark';
 import Kanban from './components/kanban/Kanban';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
 	return (
 		<>
+			<GlobalStyle />
+			<Bookmark />
 			<Kanban />
 		</>
 	);
